f09465965a2450513c7b724220e280350c159a4a
BrayanBV <vg24003@ues.edu.sv>
2024-05-04 14:59:20 -0600

    jodido

---
+++ demo-vg24003BV/src/main/java/com/iis/vg24003/demo_vg24003BV/App.java
@@ -0,0 +1,17 @@
+package com.iis.vg24003.demo_vg24003BV;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String  Hello() {
+		return "Hello world";
+	}
+    public static void main( String[] args )
+    {
+    	App hw = new App();
+        System.out.println( hw.Hello() );
+    }
+}
